3e106804e7ca492a9b20bac803ef1e16e3465941
Klacik <l3mon.off@gmail.com>
2021-12-22 16:17:56 +0100

    scipt for selling and shiny dust

---
+++ script.py
@@ -0,0 +1,43 @@
+from pynput import keyboard
+import pyautogui
+
+pyautogui.PAUSE = 0.01
+
+
+def on_activate_stop():
+    print('Exiting...')
+    exit()
+
+
+def on_activate_sell():
+    print('sell')
+    x, y = 1370, 640
+    step = 85
+    pyautogui.moveTo(x, y)
+    for row in range(4):
+        for column in range(7):
+            pyautogui.click(x+column*step, y+row*step, button='right')
+    pyautogui.keyUp('`')
+    pyautogui.keyUp('alt')
+
+
+def on_activate_shiny_dust():
+    print('shiny dust')
+    gem_x, gem_y = pyautogui.position()
+    pyautogui.click(x=100, y=750, button='right')
+    pyautogui.click(x=gem_x, y=gem_y, button='right')
+    pyautogui.click(x=200, y=400)
+    pyautogui.click(x=550, y=820)
+    pyautogui.click(x=540, y=800)
+    pyautogui.click(x=700, y=700)
+    pyautogui.click(x=gem_x, y=gem_y, button='right')
+    pyautogui.keyUp('`')
+    pyautogui.keyUp('ctrl')
+
+
+with keyboard.GlobalHotKeys({
+    '<alt>+<ctrl>+c': on_activate_stop,
+    '<ctrl>+`': on_activate_shiny_dust,
+    '<alt>+`': on_activate_sell
+        }) as hotkey:
+    hotkey.join()
